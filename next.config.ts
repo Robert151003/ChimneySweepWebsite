067cc3eb123422ca93b27e3ef9b856f715086325
Robert Forsythe <robert.forsythe1234@gmail.com>
2025-04-18 21:43:24 +0100

    update config

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
-
-/*import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
+  images:{
+    unoptimized:true,
+  },
   output: 'export', // 👈 enables static export
 };
 
 export default nextConfig;
-*/
